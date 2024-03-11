d4d35b3cc354ebe617f157ac4ab30a65dee2fbac
Vasilis Samoladas <vsam@softnet.tuc.gr>
2024-03-11 14:55:25 +0200

    Added pytest support

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+# 
+# Central pytest configuration
+# -----------------------------
+#
+# This is the location for declaring fixtures and other utilities that are auto-included
+# in test files.
+
+import pytest
+
