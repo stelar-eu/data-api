de667ec97341b09f709ba8f600301973df626907
alexZeakis <alzeakis@gmail.com>
2024-04-01 12:19:07 +0300

    Minor fixes.

--- src/container_utils.py
+++ src/container_utils.py
@@ -1,17 +1,12 @@
 import docker
 
-def create_container(docker_image, token, api_url, task_exec_id, platform='docker'):
-    if platform == 'docker':
-        client = docker.from_env()
-        container = client.containers.run(
-            docker_image,  # Image name
-            [token, api_url, task_exec_id],
-            # volumes={logdir: {'bind': '/app/logs/', 'mode': 'rw'}},
-            detach=True
-        )
-        
-        return container.id
-    elif platform == 'kubernetes':
-        return None
-    else:
-        raise ValueError("Not acceptable value")
+def create_container(docker_image, token, api_url, task_exec_id):
+    client = docker.from_env()
+    container = client.containers.run(
+        docker_image,  # Image name
+        [token, api_url, task_exec_id],
+        # volumes={logdir: {'bind': '/app/logs/', 'mode': 'rw'}},
+        detach=True
+    )
+    
+    return container.id
