66eb40d85ea0f8c811efe2f05b6fcc93efc4b16b
Dimitris Petrou <petroudimitris@outlook.com>
2025-03-12 01:25:53 +0200

    A subset of the task endpoint utilize the newly added TASK singleton

--- src/routes/workflows.py
+++ src/routes/workflows.py
@@ -7,6 +7,7 @@ import schema
 import tools
 import wflow
 import wxutils
+import kutils
 from auth import token_active
 
 # Input schema for validating and structuring several API requests
@@ -14,9 +15,7 @@ from routes.generic import generate_endpoints, render_api_output
 
 logger = logging.getLogger(__name__)
 
-workflows_bp = APIBlueprint(
-    "rest_workflows_blueprint", __name__, tag="RESTful Workflow Operations"
-)
+workflows_bp = APIBlueprint("rest_workflows_blueprint", __name__, tag="Task Operations")
 
 # --------------------------------------------------------
 # ---------------------- WORKFLOWS -----------------------
@@ -33,8 +32,8 @@ generate_endpoints(wflow.WORKFLOW, workflows_bp, logger)
 # --------------------------------------------------------
 
 
-@workflows_bp.route("/tasks/<task_id>", methods=["GET"])
-@workflows_bp.doc(tags=["RESTful Workflow Operations"])
+@workflows_bp.route("/task/<task_id>", methods=["GET"])
+@workflows_bp.doc(tags=["Task Operations"])
 @workflows_bp.output(schema.ResponseAmbiguous, status_code=200)
 @token_active
 @render_api_output(logger)
@@ -50,64 +49,31 @@ def api_get_task_metadata(task_id):
         - 404: Task is not found
         - 500: An unknown error occurred
     """
-    resp = wxutils.TASK.get_entity(task_id)
-    return resp
+    return wxutils.TASK.get_entity(task_id)
 
 
-@workflows_bp.route("/tasks", methods=["POST"])
-@workflows_bp.doc(tags=["RESTful Workflow Operations"])
-@workflows_bp.input(schema.Task_Input_v2, location="json")
-@workflows_bp.output(schema.ResponseAmbiguous, status_code=200)
+@workflows_bp.route("/task", methods=["POST"])
+@workflows_bp.doc(tags=["Task Operations"])
+@workflows_bp.input(wxutils.TaskSchema, location="json")
+@workflows_bp.output(schema.APIResponse, status_code=200)
 @token_active
+@render_api_output(logger)
 def api_rest_create_task(json_data):
     """Create a new Task Execution in the Workflow Execution Engine.
     Args:
-        task_spec: The validated JSON input containing task metadata.
-
+        task_spec: The validated JSON input containing task spec.
     Returns:
         A JSON response containing success status, the newly created task, or error details.
-    Responses:
-        - 200: Task successfully created and returned.
-        - 400: Missing required metadata or invalid parameters.
-        - 403: Workflow already committed.
-        - 500: An unknown error occurred.
     """
-    try:
-        access_token = request.headers.get("Authorization").split(" ")[1]
-        resp = wxutils.create_task(json_data, access_token)
-        return {"success": True, "result": {"task": resp}, "help": request.url}, 200
-
-    except ValueError as ve:
-        return {
-            "success": False,
-            "error": {"name": f"Error: {ve}", "__type": "Task Creation Error"},
-            "help": request.url,
-        }, 400
-    except AttributeError as e:
-        return {
-            "help": request.url,
-            "error": {
-                "name": f"Error: {e}",
-                "__type": "Workflow Already Committed Error",
-            },
-            "success": False,
-        }, 403
-    except RuntimeError as e:
-        return {
-            "help": request.url,
-            "error": {
-                "name": f"Error: {e}",
-                "__type": "Task Creation Runtime Error",
-            },
-            "success": False,
-        }, 500
+    return wxutils.TASK.create_entity(json_data)
 
 
-@workflows_bp.route("/tasks/<task_id>/input", methods=["GET"])
-@workflows_bp.route("/tasks/<task_id>/<signature>/input", methods=["GET"])
-@workflows_bp.doc(tags=["RESTful Workflow Operations"])
-@workflows_bp.output(schema.ResponseAmbiguous, status_code=200)
+@workflows_bp.route("/task/<task_id>/input", methods=["GET"])
+@workflows_bp.route("/task/<task_id>/<signature>/input", methods=["GET"])
+@workflows_bp.doc(tags=["Task Operations"])
+@workflows_bp.output(schema.APIResponse, status_code=200)
 @token_active
+@render_api_output(logger)
 def api_rest_get_task_input(task_id, signature=None):
     """Return the input JSON of the specific Task Execution.
 
@@ -117,48 +83,13 @@ def api_rest_get_task_input(task_id, signature=None):
         id: The unique identifier of the Task Execution.
 
     Returns:
-        A JSON with the inputs, parameters & MinIO credentials
-
-    Responses:
-        - 200: Task input JSON successfully returned.
-        - 400: Task ID is not valid.
-        - 404: Task is not found.
-        - 500: An unknown error occurred.
+        A JSON with the inputs, outputs, parameters & MinIO credentials
     """
-    try:
-        access_token = request.headers.get("Authorization").split(" ")[1]
-        resp = wxutils.get_task_input_json(
-            task_id=task_id, signature=signature, access_token=access_token
-        )
-        return {"success": True, "result": resp, "help": ""}, 200
-    except ValueError as ve:
-        return {
-            "success": False,
-            "error": {"name": f"Error: {ve}", "__type": "Task Not Found"},
-            "help": "",
-        }, 404
-    except AttributeError as e:
-        return {
-            "help": "",
-            "error": {
-                "name": f"Error: {e}",
-                "__type": "Not valid Task ID",
-            },
-            "success": False,
-        }, 400
-    except RuntimeError as e:
-        return {
-            "help": "",
-            "error": {
-                "name": f"Error: {e}",
-                "__type": "Task Fetch Runtime Error",
-            },
-            "success": False,
-        }, 500
+    return wxutils.TASK.get_input(task_id, kutils.current_token(), signature=signature)
 
 
-@workflows_bp.route("/tasks/<task_id>/<signature>/output", methods=["POST"])
-@workflows_bp.doc(tags=["RESTful Workflow Operations"])
+@workflows_bp.route("/task/<task_id>/<signature>/output", methods=["POST"])
+@workflows_bp.doc(tags=["Task Operations"])
 @workflows_bp.input(schema.Task_Output, location="json")
 @workflows_bp.output(schema.ResponseAmbiguous, status_code=200)
 def api_rest_post_task_output(task_id, signature, json_data):
@@ -223,8 +154,8 @@ def api_rest_post_task_output(task_id, signature, json_data):
         }, 500
 
 
-@workflows_bp.route("/tasks/<task_id>/logs", methods=["GET"])
-@workflows_bp.doc(tags=["RESTful Workflow Operations"])
+@workflows_bp.route("/task/<task_id>/logs", methods=["GET"])
+@workflows_bp.doc(tags=["Task Operations"])
 @workflows_bp.output(schema.ResponseAmbiguous, status_code=200)
 @token_active
 def api_get_task_logs(task_id):
@@ -254,8 +185,8 @@ def api_get_task_logs(task_id):
         }, 500
 
 
-@workflows_bp.route("/tasks/<task_id>/jobs", methods=["GET"])
-@workflows_bp.doc(tags=["RESTful Workflow Operations"])
+@workflows_bp.route("/task/<task_id>/jobs", methods=["GET"])
+@workflows_bp.doc(tags=["Task Operations"])
 @workflows_bp.output(schema.ResponseAmbiguous, status_code=200)
 @token_active
 def api_get_task_jobs(task_id):
@@ -286,11 +217,12 @@ def api_get_task_jobs(task_id):
         }, 500
 
 
-@workflows_bp.route("/tasks/<task_id>", methods=["DELETE"])
-@workflows_bp.doc(tags=["RESTful Workflow Operations"])
+@workflows_bp.route("/task/<entity_id>", methods=["DELETE"])
+@workflows_bp.doc(tags=["Task Operations"])
 @workflows_bp.output(schema.ResponseAmbiguous, status_code=200)
 @token_active
-def api_delete_task(task_id):
+@render_api_output(logger)
+def api_delete_task(entity_id):
     """Delete a Task Execution from the Workflow Execution Engine.
     Args:
         task_id: The unique identifier of the Task Execution.
@@ -301,36 +233,20 @@ def api_delete_task(task_id):
         - 404: Task not found.
         - 500: An unknown error occurred.
     """
-    try:
-        resp = wxutils.delete_task(task_id)
-        return {"success": True, "result": {task_id: resp}, "help": request.url}, 200
-    except ValueError as ve:
-        return {
-            "success": False,
-            "error": {"name": f"Error: {ve}", "__type": "Task Not Found Error"},
-            "help": request.url,
-        }, 404
-    except Exception as e:
-        return {
-            "help": request.url,
-            "error": {
-                "name": f"Error: {e}",
-                "__type": "Unknown Error",
-            },
-            "success": False,
-        }, 500
+    resp = wxutils.TASK.delete_entity(entity_id)
+    return resp
 
 
-@workflows_bp.route("/tasks/<task_id>", methods=["PATCH"])
+@workflows_bp.route("/task/<entity_id>", methods=["PATCH"])
 @workflows_bp.input(schema.WorkflowState, location="json")
-@workflows_bp.doc(tags=["RESTful Workflow Operations"])
+@workflows_bp.doc(tags=["Task Operations"])
 @workflows_bp.output(schema.ResponseAmbiguous, status_code=200)
 @token_active
-def api_update_task_state(task_id, json_data):
+@render_api_output(logger)
+def api_update_task_state(entity_id, json_data):
     """Update the state of a task given its unique identifier.
     Args:
         task_id: The unique identifier of the Task Execution.
-        json_data: The validated JSON input containing the new state of the task. Only the state field is required.
         The state must be one of the following: 'failed', 'succeeded', 'running'.
     Returns:
         A JSON response containing success status, or error
@@ -339,38 +255,4 @@ def api_update_task_state(task_id, json_data):
         - 404: Task not found.
         - 500: An unknown error occurred.
     """
-    try:
-        is_updated, state = wxutils.update_task_state(task_id, json_data.get("state"))
-        if is_updated:
-            return {
-                "success": True,
-                "result": {"task_id": task_id, "state": state},
-                "help": request.url,
-            }, 200
-        else:
-            return {
-                "help": request.url,
-                "error": {
-                    "name": f"Error: Task State {state} not valid",
-                    "__type": "Task State Update Error",
-                },
-                "success": False,
-            }, 400
-    except ValueError as ve:
-        return {
-            "help": request.url,
-            "error": {
-                "name": f"Error: {ve}",
-                "__type": "Task Not Found Error",
-            },
-            "success": False,
-        }, 404
-    except Exception as e:
-        return {
-            "help": request.url,
-            "error": {
-                "name": f"Error: {e}",
-                "__type": "Unknown Error",
-            },
-            "success": False,
-        }, 500
+    return wxutils.TASK.patch_entity(entity_id, json_data.get("state"))
