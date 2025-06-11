56d443a250617c16982ac7981e82cab2af4fa074
Dimitris Petrou <petroudimitris@outlook.com>
2025-06-11 18:39:49 +0300

    Created a backend for KG  and create a new endpoint that is authorizing SPARQL queries to OnTop. The x-user header arriving to OnTop gets populated with the UUID of the caller user. This allows for the OnTop Mapping to evaluate access by exposing entities that are only accessible by the user by looking at the view.

---
+++ src/backend/kg.py
@@ -0,0 +1,52 @@
+import requests
+from flask import current_app
+import logging
+from authz_module import get_user_organizations
+import kutils
+
+logger = logging.getLogger(__name__)
+
+
+class OntopClient:
+    def __init__(self, endpoint_url):
+        """
+        Initialize the SPARQL client with the endpoint URL.
+        """
+        self.endpoint_url = endpoint_url
+        self.default_headers = {
+            "Content-Type": "application/sparql-query",
+            "Accept": "application/json",
+        }
+
+    @classmethod
+    def get_client(cls):
+        """
+        Create and return an OntopClient instance using the Flask app context.
+        """
+        app = current_app._get_current_object()
+        with app.app_context():
+            config = app.config["settings"]
+            endpoint_url = config["SPARQL_ENDPOINT"]
+            return cls(endpoint_url)
+
+    def execute_query(self, sparql_query):
+        """
+        Execute a SPARQL query against the endpoint with the specified user header.
+        """
+        headers = self.default_headers.copy()
+        headers["x-user"] = kutils.current_user()["sub"]
+
+        try:
+            logger.debug(f"Sending SPARQL query with headers: {headers}")
+            response = requests.post(
+                self.endpoint_url, headers=headers, data=sparql_query
+            )
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error executing SPARQL query: {e}")
+            return None
+
+
+# The Ontop client selector
+ONTOP_CLIENT = OntopClient.get_client
